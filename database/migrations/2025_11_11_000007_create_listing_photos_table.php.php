37a3973e672f906f5cd9397134f9280dda6f1739
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-09 14:19:07 +0200

    Update 2025_11_11_000007_create_listing_photos_table.php.php

--- database/migrations/2025_11_11_000007_create_listing_photos_table.php.php
+++ database/migrations/2025_11_11_000007_create_listing_photos_table.php.php
@@ -7,19 +7,20 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('listingPhoto', function (Blueprint $table) {
+        Schema::create('listing_photos', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('listing_id');
-            $table->string('failo_url', 255);
+            $table->string('failo_url');
             $table->timestamps();
-
-            $table->foreign('listing_id')->references('id')->on('listing')->onDelete('cascade');
+            $table->foreign('listing_id')
+                  ->references('id')
+                  ->on('listing')
+                  ->onDelete('cascade');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('listingPhoto');
+        Schema::dropIfExists('listing_photos');
     }
 };
-
