ff5abe7d7f7b89e027e4ba528377514926d242bb
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-09 13:29:04 +0200

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,12 +10,31 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Category::upsert([
-            ['pavadinimas' => 'Electronics'],
-            ['pavadinimas' => 'Furniture'],
-            ['pavadinimas' => 'Clothing'],
-            ['pavadinimas' => 'Books'],
-            ['pavadinimas' => 'Home & Kitchen'],
-            ['pavadinimas' => 'Sports'],
-        ], ['pavadinimas']);
+            [
+                'pavadinimas' => 'Keramika',
+                'aprasymas' => 'Rankų darbo molio gaminiai',
+                'tipo_zenklas' => 'preke',
+            ],
+            [
+                'pavadinimas' => 'Mediena',
+                'aprasymas' => 'Rankų darbo mediniai gaminiai',
+                'tipo_zenklas' => 'preke',
+            ],
+            [
+                'pavadinimas' => 'Papuošalai',
+                'aprasymas' => 'Rankų darbo papuošalai',
+                'tipo_zenklas' => 'preke',
+            ],
+            [
+                'pavadinimas' => 'Tekstilė',
+                'aprasymas' => 'Rankų darbo tekstilės gaminiai',
+                'tipo_zenklas' => 'preke',
+            ],
+            [
+                'pavadinimas' => 'Skaitmeniniai',
+                'aprasymas' => 'Skaitmeniniai produktai',
+                'tipo_zenklas' => 'paslauga',
+            ],
+        ], ['pavadinimas'], ['aprasymas', 'tipo_zenklas']);
     }
 }
