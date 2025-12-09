5f13cb59adb7e821736b695e39126b3608a998d1
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-09 20:58:54 +0200

    Update Favorite.php

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -18,7 +18,7 @@ class Favorite extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function Listing()
+    public function listing()
     {
         return $this->belongsTo(Listing::class);
     }
