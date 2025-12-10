b0a0d90d35473e5e1a88b8e3770e743a99272e91
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-10 17:59:50 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,11 +58,20 @@ class User extends Authenticatable
         return $this->hasMany(Cart::class);
     }
 
-   public function favorites()
-{
-    return $this->hasMany(\App\Models\Favorite::class);
-}
+ public function Favorite()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 
+    public function favoriteListings()
+{
+    return $this->belongsToMany(
+        Listing::class,
+        'favorite',
+        'user_id',
+        'listing_id'
+    )->withTimestamps();
+}
 
     public function Order()
     {
