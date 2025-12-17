c9d87125c2f4bfa15c0f4d05d9f63f1c040b08e8
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-17 21:40:15 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; 
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Frontend\HomeSearchController;
