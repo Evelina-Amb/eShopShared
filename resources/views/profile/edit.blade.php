86a9e4ba9a1115e54f3a0f50de243c71948bcd5a
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-17 17:31:00 +0200

    Update edit.blade.php

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,12 +1,5 @@
 <x-app-layout>
     <div class="py-12">
-        @if(auth()->user()->role === 'seller' && auth()->user()->stripe_onboarded)
-    <a href="{{ route('stripe.dashboard') }}"
-       class="btn btn-outline-primary"
-       target="_blank">
-        View Stripe earnings
-    </a>
-@endif
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg max-w-2xl w-full mx-auto">
                 <div class="w-full">
