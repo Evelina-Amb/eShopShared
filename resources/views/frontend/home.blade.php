697b393d079ad94d02c5c6102d8a69be275139d6
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-28 07:40:34 -0800

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -30,7 +30,7 @@
                     <button
     type="button"
     @click.prevent="Alpine.store('favorites').toggle({{ $item->id }})"
-    class="absolute top-2 right-2 z-40 w-8 h-8 flex items-center justify-center overflow-hidden"
+    class="absolute top-2 right-2 z-50 flex items-center justify-center overflow-hidden"
     aria-label="Toggle favorite"
 >
     <span
