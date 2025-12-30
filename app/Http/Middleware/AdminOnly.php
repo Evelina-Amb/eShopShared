ab012387c5f3b3eeb10ad0392f888fac1ec52743
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-12-30 19:17:57 +0200

    Update AdminOnly.php

--- app/Http/Middleware/AdminOnly.php
+++ app/Http/Middleware/AdminOnly.php
@@ -4,23 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class AdminOnly
 {
-    public function handle($request, Closure $next)
-{
-    // temp: use ?admin_id=1 in Postman
-$user = \App\Models\User::find($request->admin_id);
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check() || auth()->user()->role !== 'admin') {
+            abort(403);
+        }
 
-if (!$user || $user->role !== 'admin') {
-    return response()->json([
-        'status' => 'error',
-        'message' => 'Only admin can perform this action.'
-    ], 403);
-}
-
-    return $next($request);
-}
-
+        return $next($request);
+    }
 }
