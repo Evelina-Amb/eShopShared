0e30298f9e33040b849e6a46a375d2ce4a8152bd
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-05 18:06:22 +0200

    Update home.blade.php

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -3,9 +3,9 @@
 <div
     x-data
     x-init="Alpine.store('favorites').load()"
-    class="container mx-auto px-3 sm:px-4 mt-6 sm:mt-8 max-w-7xl"
+    class="container mx-auto px-3 sm:px-4 mt-6 sm:mt-8"
 >
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6">
+    <div class="grid grid-cols-[repeat(auto-fit,minmax(260px,1fr))] gap-4 sm:gap-6">
 
         @forelse ($listings as $item)
             <div class="bg-white shadow rounded overflow-hidden hover:shadow-lg transition flex flex-col">
