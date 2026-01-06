b9bfea408705bd814c6279a9c77aa2b67ceb1a4c
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-06 01:50:57 -0800

    Add files via upload

---
+++ app/Mail/SellerNewOrderMail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SellerNewOrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public Order $order,
+        public User $seller,
+        public array $items,
+        public array $shipping
+    ) {}
+
+    public function build()
+    {
+        return $this->subject('New sale: Order #' . $this->order->id)
+            ->view('emails.seller-new-order');
+    }
+}
