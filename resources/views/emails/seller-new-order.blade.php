08f94ba83511451c4aa5de74314dc4415c1eac6d
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-06 02:18:59 -0800

    Add files via upload

---
+++ resources/views/emails/seller-new-order.blade.php
@@ -0,0 +1,33 @@
+@component('mail::message')
+# New Sale Received 🎉
+
+Hello {{ $seller->vardas }},
+
+You have a new sale in **Order #{{ $order->id }}**.
+
+---
+
+## Items to ship
+@foreach($items as $it)
+- **{{ $it['title'] }}** × {{ $it['qty'] }}
+@endforeach
+
+---
+
+## Shipping address
+{{ $shipping['address_line'] }}  
+{{ $shipping['city'] }}, {{ $shipping['country'] }} {{ $shipping['postal_code'] }}
+
+---
+
+## Shipment deadline  **{{ $shipping['deadline'] }}**
+
+---
+
+@component('mail::button', ['url' => $shipping['shipments_url']])
+Manage Shipment
+@endcomponent
+
+Thank you,  
+{{ config('app.name') }}
+@endcomponent
