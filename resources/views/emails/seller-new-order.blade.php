82ffedcb5be824d9f3de15c3a827ceef89fbbe02
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-06 12:34:20 +0200

    Update seller-new-order.blade.php

--- resources/views/emails/seller-new-order.blade.php
+++ resources/views/emails/seller-new-order.blade.php
@@ -8,8 +8,27 @@ You have a new sale in **Order #{{ $order->id }}**.
 ---
 
 ## Items to ship
+
 @foreach($items as $it)
-- **{{ $it['title'] }}** × {{ $it['qty'] }}
+<table width="100%" cellpadding="0" cellspacing="0" style="margin-bottom:12px;">
+<tr>
+<td style="vertical-align:top;">
+<strong>{{ $it['title'] }}</strong> × {{ $it['qty'] }}
+</td>
+
+<td align="right" style="width:80px;">
+@if($it['image'])
+<img
+    src="{{ $it['image'] }}"
+    width="70"
+    height="70"
+    style="object-fit:cover;border-radius:6px;border:1px solid #ddd;"
+    alt="{{ $it['title'] }}"
+>
+@endif
+</td>
+</tr>
+</table>
 @endforeach
 
 ---
