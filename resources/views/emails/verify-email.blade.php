422e60344b7a1ae0e7b688d077ef987e422e42c7
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2026-01-07 15:25:43 +0200

    Update verify-email.blade.php

--- resources/views/emails/verify-email.blade.php
+++ resources/views/emails/verify-email.blade.php
@@ -1,7 +1,7 @@
-<p>Hello,</p>
+<p>Sveiki,</p>
 
-<p>Please confirm your email to complete your registration.</p>
+<p>Prašome patvirtinti savo el. pašto adresą, kad užbaigtumėte registraciją.</p>
 
 <p>
-    <a href="{{ $url }}">Click here to verify your email</a>
+    <a href="{{ $url }}">Spauskite čia, kad patvirtintumėte savo el. pašto adresą</a>
 </p>
